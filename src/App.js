3482a68def4334cb611599148a21432e9781639d
gregory kgosiejang <gregorykgosiejang0@gmail.com>
2023-10-24 16:20:35 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Start from "./components/Start";
+import SignUp from "./components/SignUp";
+
+
+function App() {
+  return (
+    <div>
+      <Start/>
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SignUp.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+class SignUp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return (
+        <div>
+            <h1>Sign Up</h1>
+            <p>Sign up page</p>
+        </div>
+        );
+    }
+    }
+
+export default SignUp;
